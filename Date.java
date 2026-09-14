f773d08fd186b4bc3cde613b93986a8b2a3e7296
JackTHEgenie <Jackmanfeller@icloud.com>
2026-09-14 14:55:59 -0700

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,14 @@
+public class Date {
+
+ public static void main(String[] args) {
+ String day = "Monday";
+ String date = "14th";
+ String month = " September ";
+ String year = " 2026";
+ System.out.println(day + "," + month + date + "," + year + ".");
+ //this structure is the start of most code
+ 
+ 
+ }
+ }
+ 
