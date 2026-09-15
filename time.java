65a9269e1b6eb76cd1f44aaec7fd38f4a2b0e970
JackTHEgenie <Jackmanfeller@icloud.com>
2026-09-15 15:01:36 -0700

    Add files via upload

---
+++ time.java
@@ -0,0 +1,12 @@
+public class time {
+public static void main(String[] args) {
+	
+	int hour = 14;
+	int minute = 45;
+	int second = 00;
+System.out.print("Number of seconds since midnight: " + hour * 3600 + minute * 60);
+System.out.print("Number of seconds remaining in the day: " + hour);
+System.out.print("Percentage of day that has passed: " );
+
+}
+}
